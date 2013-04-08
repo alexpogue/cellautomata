8287c77e06534d3e8e602232a31698cae8017f54
Alex Pogue <alex@epogue.com>
2013-04-08 00:48:39 -0500

    Added preprocessing code. One test still does not pass.

--- src/AutPreprocessor.cpp
+++ src/AutPreprocessor.cpp
@@ -1,6 +1,103 @@
 #include "AutPreprocessor.h"
 #include <string>
+#include <iostream>
+
+size_t getNextNonWhite(const std::string& autText, size_t pos);
+size_t removeLeadingWhitespace(std::string& autText, size_t pos);
+size_t preprocessKeyword(std::string& autText, size_t pos);
+size_t removeLine(std::string& autText, size_t pos);
+size_t getNextNewline(const std::string& autText, size_t pos);
+size_t getNextKeywordEnding(const std::string& autText, size_t pos);
+bool handleComment(std::string& autText, size_t pos);
+std::string getWhitespaceChars();
+bool isWhitespace(char possible);
 
 std::string AutPreprocessor::preprocess(std::string rawAutText) {
-  return "not implemented";
+  size_t pos = 0;
+  for(;;){
+    removeLeadingWhitespace(rawAutText, pos);
+    if(pos > rawAutText.size()) {
+      break;
+    }
+    if(handleComment(rawAutText, pos)) {
+      // must remove whitespace again if we handled a comment
+      continue;
+    }
+    if(pos > rawAutText.size()) {
+      break;
+    }
+    pos = preprocessKeyword(rawAutText, pos);
+    if(pos > rawAutText.size()) {
+      break;
+    }
+  }
+  return rawAutText;
+}
+
+size_t removeLeadingWhitespace(std::string& autText, size_t pos) {
+  size_t nonWhitePos = getNextNonWhite(autText, pos);
+  if(nonWhitePos == std::string::npos) {
+    nonWhitePos = autText.size();
+  }
+  size_t whiteSpaceLength = nonWhitePos - pos;
+  autText.erase(pos, whiteSpaceLength);
+  return whiteSpaceLength;
+}
+
+size_t getNextNonWhite(const std::string& autText, size_t pos) {
+  return autText.find_first_not_of(getWhitespaceChars(), pos);
+}
+
+std::string getWhitespaceChars() {
+  return " \t\f\v\n\r";
+}
+
+bool handleComment(std::string& autText, size_t pos) {
+  if(autText[pos] == '#') {
+    removeLine(autText, pos);
+    return true;
+  }
+  return false;
+}
+
+size_t preprocessKeyword(std::string& autText, size_t pos) {
+  size_t keywordEndingPos = getNextKeywordEnding(autText, pos);
+  if(keywordEndingPos == std::string::npos) {
+    keywordEndingPos = autText.size();
+  }
+  for(; pos < keywordEndingPos; pos++) {
+    if(autText[pos] == '#') {
+      keywordEndingPos -= removeLine(autText, pos);
+    }
+    else if(isWhitespace(autText[pos])) {
+      autText.replace(pos, 1, " ");
+      keywordEndingPos -= removeLeadingWhitespace(autText, pos+1); 
+    }
+  }
+  if(isWhitespace(autText[keywordEndingPos - 1])) {
+    autText.erase(keywordEndingPos - 1, 1);
+  }
+  return pos + 1;
+}
+
+bool isWhitespace(char possible) {
+  return getWhitespaceChars().find(std::string(1,possible)) != std::string::npos;
+}
+
+size_t removeLine(std::string& autText, size_t pos) {
+  size_t charAfterNewlinePos = getNextNewline(autText, pos) + 1;
+  if(charAfterNewlinePos == std::string::npos) {
+    charAfterNewlinePos = autText.size();
+  }
+  size_t lineLength = charAfterNewlinePos - pos;
+  autText.erase(pos, lineLength);
+  return lineLength;
+}
+
+size_t getNextNewline(const std::string& autText, size_t pos) {
+  return autText.find_first_of("\n", pos);
+}
+
+size_t getNextKeywordEnding(const std::string& autText, size_t pos) {
+  return autText.find_first_of(";{", pos);
 }
--- src/AutPreprocessorTest.cpp
+++ src/AutPreprocessorTest.cpp
@@ -3,12 +3,49 @@
 #include <boost/test/unit_test.hpp>
 #include <string>
 #include <vector>
+#include <iostream>
 #include "AutPreprocessor.h"
 
 void checkStringArrayMatches(std::string model, std::string* arr, int size) {
   for(int i = 0; i < size; i++) {
     BOOST_CHECK_EQUAL(model, arr[i]);
   }
+}
+
+BOOST_AUTO_TEST_CASE(cornerCases) {
+  std::string singleSemicolon = AutPreprocessor::preprocess(";");
+  std::string singleCurly = AutPreprocessor::preprocess("{");
+  std::string empty = AutPreprocessor::preprocess("");
+  std::string singleChar = AutPreprocessor::preprocess("c");
+  std::string singleWhitespace = AutPreprocessor::preprocess(" ");
+  BOOST_CHECK_EQUAL(";", singleSemicolon);
+  BOOST_CHECK_EQUAL("{", singleCurly);
+  BOOST_CHECK_EQUAL("", empty);
+  BOOST_CHECK_EQUAL("c", singleChar);
+  BOOST_CHECK_EQUAL("", singleWhitespace);
+}
+
+BOOST_AUTO_TEST_CASE(statements) {
+  std::string lotsOfWhite = AutPreprocessor::preprocess("h  e\t  \n  l\r l \vo\fworld;\t\n\r\f\vs        ec    on  dke   ywo  rd;\t  \n");
+  std::string lotsOfWhiteExpected = "h e l l o world;s ec on dke ywo rd;";
+  std::string endCurlies = AutPreprocessor::preprocess("out{inner;inner;};next{nextinner;};");
+  std::string endCurliesExpected = "out{inner;inner;};next{nextinner;};";
+  std::string endCurliesWithSpaces = AutPreprocessor::preprocess("out  {  inner;  inner;  }  ;  next{nextinner  ;  }  ;  ");
+  BOOST_CHECK_EQUAL(lotsOfWhiteExpected, lotsOfWhite);
+  BOOST_CHECK_EQUAL(endCurlies, endCurliesExpected);
+  BOOST_CHECK_EQUAL(endCurliesWithSpaces, endCurliesExpected);
+}
+
+BOOST_AUTO_TEST_CASE(leadingSpace) {
+  std::string leadingSpacesExpected = "hi;";
+  std::string leadingSpace = AutPreprocessor::preprocess(" hi;");
+  std::string leadingSpaces = AutPreprocessor::preprocess("  hi;");
+  std::string leadingNewlines = AutPreprocessor::preprocess("\n\nhi;");
+  std::string leadingCrs = AutPreprocessor::preprocess("\r\rhi;");
+  std::string leadingTabs = AutPreprocessor::preprocess("\t\thi;");
+  std::string leadingComboWhite = AutPreprocessor::preprocess(" \t\r\n hi;");
+  std::string leadingSpacesStrs[] = {leadingSpace, leadingSpaces, leadingNewlines, leadingCrs, leadingTabs, leadingComboWhite};
+  checkStringArrayMatches(leadingSpacesExpected, leadingSpacesStrs, 6);
 }
 
 BOOST_AUTO_TEST_CASE(spcSemicolon) {
@@ -64,6 +101,7 @@ BOOST_AUTO_TEST_CASE(comments) {
   std::string commentWithSemi = AutPreprocessor::preprocess("hi;#;invisible;");
   std::string commentWithCurly = AutPreprocessor::preprocess("hi;#{invisible;");
   std::string commentWithComment = AutPreprocessor::preprocess("hi;##invisible");
+  std::string commentInsideKeyword = AutPreprocessor::preprocess("hi#comment\n;");
   std::string commentStrs[] = {commentWithSemi, commentWithCurly, commentWithComment};
   checkStringArrayMatches(commentsExpected, commentStrs, 3);
 }
