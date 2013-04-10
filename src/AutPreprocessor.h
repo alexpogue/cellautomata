410d1c0f6a52f52f9ec8a156350953b22aa5232f
Alex Pogue <alex@epogue.com>
2013-04-10 15:25:54 -0500

    Added AutPreprocessor::process() documentation

--- src/AutPreprocessor.h
+++ src/AutPreprocessor.h
@@ -5,6 +5,17 @@
 
 class AutPreprocessor {
   public:
+    /** 
+    Preprocesses rawAutText string such that the returned string is rawAutText
+    with all extra spaces (defined below) and comments removed. Any whitespace 
+    between keywords is replaced with a single space.
+
+    Extra spaces refers to any whitespace not within a keyword, or all spaces
+    within a keyword after the first space. For example, the extra spaces are  
+    obviated in the string: "  Hello   world ; "
+                             ^^      ^^     ^ ^
+    Resulting in the output: "Hello world;"
+     */
     static std::string preprocess(std::string rawAutText);
 };
 
